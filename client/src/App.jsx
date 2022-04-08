d03265f9673c871e344cd7b11dfff9c96d3722be
migiburgos <67326096+migiburgos@users.noreply.github.com>
2022-04-08 09:13:51 +0300

    Created Folder Structure

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        {/* Header */}
+        {/* Navbar */}
+      </div>
+      {/* About */}
+      {/* Projects */}
+      {/* Contact */}
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// // eslint-disable-next-line
+// import "swiper/css/bundle";
+// import "./styles.css";
+
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
