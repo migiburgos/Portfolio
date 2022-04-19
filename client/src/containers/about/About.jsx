2098f5f24a65ff9a0c26994e321012522efbc00f
migiburgos <67326096+migiburgos@users.noreply.github.com>
2022-04-20 00:00:56 +0300

    About Prototype Completed
    - Home image is larger

--- client/src/containers/about/About.jsx
+++ client/src/containers/about/About.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./about.css";
+import mypic1 from "../../assets/mypic1.png";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about section__padding">
+      <div className="about__photo">
+        <img src={mypic1} alt="mypic1" />
+      </div>
+      <div className="about__content">
+        <h1>About Me</h1>
+        <p>
+          An inquisitive Computer Science Engineering student, skilled in
+          leadership, seeking to leverage solid development skills with focus on
+          collaboration, communication and passion.
+        </p>
+        <button type="button">Download CV</button>
+      </div>
+    </div>
+  );
 };
 
 export default About;
