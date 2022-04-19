13cb2bf31a119a33acf081be91b4fd5b21a9343c
migiburgos <67326096+migiburgos@users.noreply.github.com>
2022-04-19 23:51:23 +0300

    Home Prototype Done

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import "./navbar.css";
 
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#about">About Me</a>
+    </p>
+    <p>
+      <a href="#projects">Projects</a>
+    </p>
+  </>
+);
+
 const Navbar = () => {
   return (
-    <div>
-      <div></div>
+    <div className="navbar section__padding">
+      <div className="navbar__links">
+        <div className="navbar__logo">
+          <p>LOGO</p>
+        </div>
+        <div className="navbar__links-container">
+          <Menu />
+        </div>
+      </div>
+
+      <button type="button">Contact me</button>
     </div>
   );
 };
--- client/src/containers/header/Header.jsx
+++ client/src/containers/header/Header.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./header.css";
+import mypic1 from "../../assets/mypic1.png";
+import mypic2 from "../../assets/mypic2.png";
+import { BsFillPersonFill, BsFillEyeFill } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header__content">
+        <h2>Hello, I'm</h2>
+        <h1>Jose Miguel C. Burgos</h1>
+        <p className="header__description">
+          Freelance UI designer, Fullstack developer, & Data Miner. I create
+          seamless web experiences for end-users.
+        </p>
+        <div className="header__buttons-container">
+          <div className="header__buttons-button">
+            <p>About me</p>
+            <BsFillPersonFill size={16} />
+          </div>
+          <div className="header__buttons-button">
+            <p>Projects</p>
+            <BsFillEyeFill size={16} />
+          </div>
+        </div>
+      </div>
+
+      <div className="header__pic">
+        <img src={mypic2} alt="mypic1" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
