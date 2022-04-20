326c37db934de3519e1b0342de61b667fbd9035d
migiburgos <67326096+migiburgos@users.noreply.github.com>
2022-04-20 22:09:57 +0300

    Contact and Footer Prototype Completed

--- client/src/containers/contact/Contact.jsx
+++ client/src/containers/contact/Contact.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./contact.css";
+import { AiFillLinkedin } from "react-icons/ai";
+import { IoMdSend } from "react-icons/io";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact section__padding">
+      <h1>Contact Me</h1>
+      <p className="contact__subtitle">Get in touch</p>
+
+      <div className="contact__content">
+        <div className="contact__sideA">
+          <h2>Drop me a line.</h2>
+          <h2>I would like to hear from you.</h2>
+          <div className="contact__icons">
+            <AiFillLinkedin size={25} />
+          </div>
+        </div>
+        <div className="contact__sideB">
+          <div className="contact__info-container">
+            <input placeholder="Name" />
+            <input placeholder="Email" />
+          </div>
+          <textarea placeholder="Message" />
+          <div className="contact__button">
+            <p>Send Message</p>
+            <IoMdSend size={18} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- client/src/containers/footer/Footer.jsx
+++ client/src/containers/footer/Footer.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import "./footer.css";
+import { AiFillLinkedin } from "react-icons/ai";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#about">About Me</a>
+    </p>
+    <p>
+      <a href="#projects">Portfolio</a>
+    </p>
+    <p>
+      <a href="#contact">Contact Me</a>
+    </p>
+  </>
+);
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="sideA">
+        <div>
+          <h1>Jose</h1>
+          <p>Full-stack developer</p>
+        </div>
+      </div>
+      <div className="sideB">
+        <div className="footer_links-container">
+          <Menu />
+        </div>
+        <p>@2022 Jose Miguel Burgos. All rights reserved.</p>
+      </div>
+      <div className="sideC">
+        <AiFillLinkedin size={25} />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
